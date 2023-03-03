c06abaea7c80cac83843ad3a01e26ff917bf17d5
Seongho.park <seongho.park@nuvi-labs.com>
2023-03-03 19:29:19 +0900

    edit git info to seongho

--- frontend/landing/src/elements/Git/GitContent.jsx
+++ frontend/landing/src/elements/Git/GitContent.jsx
@@ -90,9 +90,7 @@ const GitContent = () => {
 	const [data, setData] = useState();
 	useEffect(() => {
 		axios
-			.get(
-				'https://api.github.com/users/adityassharma-ss/repos?per_page=100',
-			)
+			.get('https://api.github.com/users/psh4607/repos?per_page=100', {})
 			.then(res => {
 				let data = res.data.sort((a, b) => {
 					let x = a.stargazers_count;
